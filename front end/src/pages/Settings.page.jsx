08a51d283873e7e1faa10fbbfb4d92837d7757ce
vashishtabhinandan9 <vashishtabhinandan9@gmail.com>
2022-07-04 03:32:27 +0530

    settingspage completed

--- front end/src/pages/Settings.page.jsx
+++ front end/src/pages/Settings.page.jsx
@@ -7,34 +7,39 @@ export default function Settings() {
 <div className='flex' >
        <div className="settingsWrapper flex-auto  ml-4">
         <div className="settingsTitle mt-4 flex items-baseline">
-          <span className="settingsTitleUpdate text-red-300 text-4xl">Update Your Account</span>
-          <span className="settingsTitleDelete text-red-500  justify-end mr-4">Delete Account</span>
+          <span className="settingsTitleUpdate text-red-300 flex-auto text-4xl">Update Your Account</span>
+          <span className="settingsTitleDelete text-red-500   mr-4 cursor-pointer">Delete Account</span>
         </div>
-        <form className="settingsForm">
-          <label className='font-bold font-sans text-xl'>Profile Picture</label>
-          <div className="settingsPP mt-2  h-16 w-16 rounded-xl ">
+        <form className="settingsForm my-8">
+            
+          <label className='font-bold font-sans text-xl '>Profile Picture</label>
+          <div className="settingsPP mt-4 flex items-center gap-4   ">
             <img
-            className='rounded-xl'
+            className=' h-16 w-16 rounded-xl cursor-pointer '
               src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
               alt=""
             />
             <label htmlFor="fileInput">
-              <i className="settingsPPIcon far fa-user-circle"></i>{" "}
+              <i className="settingsPPIcon text-4xl cursor-pointer far fa-user-circle"></i>{" "}
             </label>
             <input
               id="fileInput"
               type="file"
               style={{ display: "none" }}
-              className="settingsPPInput"
+              className="settingsPPInput "
             />
+
           </div>
+          <div className='mt-4 flex flex-col w-11/12 gap-4 '> 
           <label>Username</label>
-          <input type="text" placeholder="Safak" name="name" />
+          <input className='border-b-2 border-gray-100 pl-4' type="text" placeholder="Safak" name="name" />
           <label>Email</label>
-          <input type="email" placeholder="safak@gmail.com" name="email" />
+          <input className='border-b-2 border-gray-100 pl-4' type="email" placeholder="safak@gmail.com" name="email" />
           <label>Password</label>
-          <input type="password" placeholder="Password" name="password" />
-          <button className="settingsSubmitButton" type="submit">
+          <input className='border-b-2 border-gray-100 pl-4' type="password" placeholder="Password" name="password" />
+          </div>
+          
+          <button className=" mt-8 block m-auto settingsSubmitButton bg-green-500 w-40 text-white h-12  cursor-pointer rounded-xl" type="submit">
             Update
           </button>
         </form>
