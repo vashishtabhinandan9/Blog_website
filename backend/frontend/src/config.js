5a6725a10c6a307eba31bbf9e2c575c735a022b5
vashishtabhinandan9 <vashishtabhinandan9@gmail.com>
2023-01-03 03:13:26 +0530

    vercel 7

--- backend/frontend/src/config.js
+++ backend/frontend/src/config.js
@@ -2,9 +2,11 @@ import axios from "axios"
 
 export const axiosInstance = axios.create({
 
-    baseURL:'http://localhost:5000/api/',
+    baseURL:'https://blog-website-1hek-vashishtabhinandan9.vercel.app/api',
+
     //'http://localhost:5000/api/'
     //https://daily-creative.herokuapp.com/api/
+    //https://blog-website-1hek-vashishtabhinandan9.vercel.app/api
     //timeout: 1000,
    // https://daily-creative.herokuapp.com/login
    
